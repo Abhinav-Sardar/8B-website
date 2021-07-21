b454b28c5019bf93f2f2257c7be8adbd31c3f833
Abhinav-Sardar <abhinavsardar.podar@gmail.com>
2021-07-21 15:04:46 +0530

    Final touches remaining

--- js/nicknames.js
+++ js/nicknames.js
@@ -1,5 +1,4 @@
-const nicknames = " Bubby - Abhinav |  Shanky - Shashank | Bunny - Aniruddha | Kal Ka Roop - Abhiroop | Donzie - Seema | Sauce Mein Tikka - Swastika | Baba - Sanjay | Kaka - Akash | Vibes - Vibhu | Hydra - Haider | Adi Man - Aditya Manerikar | Aja - Ajay | Amrahs Tihcra - Archit | Guppy - Yuvan | Japanese Daaku - Shiv | SS - Swastik | AdiMiss - Aditya Mishra" ; 
-// ' ' ||||||
+const nicknames = " Bubby - Abhinav |  Shanky - Shashank | Bunny - Aniruddha | Kal Ka Roop - Abhiroop | Donzie - Seema | Sauce Mein Tikka - Swastika | Baba - Sanjay | Kaka - Akash | Vibes - Vibhu | Hydra - Haider | Adi Man - Aditya Manerikar | Aja - Ajay | Amrahs Tihcra - Archit | Guppy - Yuvan | Japanese Daaku - Shiv | SS - Swastik | AdiMiss - Aditya Mishra | BLEEP - Anahita | BLEEP - Annanya | BLEEP - Arnav | Thakre - Arya | Avani - Avaneesh | BLEEP - Avani | BLEEP - Ramya | Dukhi - Khushi | Mandy - Mandisha | NASA - Naisha | hpf1re - Nilaksh | BLEEP - Parthiv | BLEEP - Renee | BLEEP - Rohit | BLEEP - Saanvi | BLEEP - Sanika | BLEEP - Sanyukta | BLEEP - Shourya " ; 
 const nicknamesArr = nicknames.split('|') ;
 const main = document.getElementById('main') ; 
 document.addEventListener('DOMContentLoaded' , () => {
